641539ad8af24369103337d4897a45bf90033107
nplatonov <nplatonov@yahoo.com>
2020-11-30 14:18:09 +0300

    3.9.2

---
+++ .gitupdate.R
@@ -0,0 +1,12 @@
+desc <- commandArgs(TRUE)
+if (!length(desc)) {
+   a <- readLines("DESCRIPTION")
+   if (length(ind <- grep("Version:",a)))
+      desc <- gsub("Version:\\s*","",a[ind])
+   else
+      desc <- "ongoing"
+}
+desc
+system("git add -A")
+#if (!system(paste0("git commit -m",dQuote(desc))))
+#   system("git push")
